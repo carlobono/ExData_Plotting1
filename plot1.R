015b9320019c2a068224e810476cb11c4983735c
email <carlo.bono@gmail.com>
2015-02-07 00:53:34 +0100

    changed red to orangered in plot1

--- plot1.R
+++ plot1.R
@@ -10,5 +10,5 @@ fdt$unixtime <- strptime(paste(fdt$Date,fdt$Time),"%d/%m/%Y %H:%M:%S")
 
 # Generate and save plot
 png(filename = "plot1.png",width = 480, height = 480)
-hist(fdt$Global_active_power,xlab="Global active power (kilowatts)",col="red",main="Global Active Power")
+hist(fdt$Global_active_power,xlab="Global active power (kilowatts)",col="orangered",main="Global Active Power")
 dev.off()
